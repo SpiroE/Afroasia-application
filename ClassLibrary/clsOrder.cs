e0afa8782bc6d0b4894d3dc39be432c8799f860f
P2657510 <P2657510@GWHXX681WIN009W>
2023-03-02 18:04:18 +0000

    Added more testing from my testing log

--- ClassLibrary/clsOrder.cs
+++ ClassLibrary/clsOrder.cs
@@ -9,11 +9,54 @@ namespace ClassLibrary
     public class clsOrder
     {
         public bool Fulfilled { get; set; }
+
+        public string FulfilledValid(bool testData)
+        {
+            if (testData != true && testData != false)
+            {
+                return "Please select true or false";
+            }
+            else
+            {
+                return "";
+            }
+        }
+
         public DateTime DateAdded { get; set; }
         public int NoOfCases { get; set; }
         public int ProductID { get; set; }
         public string Address { get; set; }
+
+        public string AddressValid(string testData)
+        {
+            if (testData.Length < 1)
+            {
+                return "The address cannot be blank";
+            }
+            if(testData.Length > 100)
+            {
+                return "The address cannot be bigger than 100 characters";
+            }
+            else
+            {
+                return "";
+            }
+        }
+
         public int CustomerID { get; set; }
         public int OrderNo { get; set; }
+
+        public string DateAddedValid(DateTime testData)
+        {
+            DateTime TestDate = DateTime.Now.Date.AddYears(-1);
+            if (testData < TestDate)
+            {
+                return "Date is more than 1 year old";
+            }
+            else
+            {
+                return "";
+            }
+        }
     }
 }
--- Testing4/tstOrder.cs
+++ Testing4/tstOrder.cs
@@ -29,9 +29,51 @@ namespace Testing4
             // test to see if the two values are the same
             Assert.AreEqual(AnOrder.Fulfilled, TestData);
         }
-        
+
         [TestMethod]
-        public void DateAddedOrder()
+        public void FulfilledValid()
+        {
+            // create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            // create some test data
+            Boolean TestData = true;
+            //create error message
+            string Error = "";
+            Error = AnOrder.FulfilledValid(TestData);
+            // test to see if the two values are the same
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void FulfilledMin()
+        {
+            // create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            // create some test data
+            Boolean TestData = false;
+            //create error message
+            string Error = "";
+            Error = AnOrder.FulfilledValid(TestData);
+            // test to see if the two values are the same
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void FulfilledMax()
+        {
+            // create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            // create some test data
+            Boolean TestData = true;
+            //create error message
+            string Error = "";
+            Error = AnOrder.FulfilledValid(TestData);
+            // test to see if the two values are the same
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void DateAddedOK()
         {
             // create an instance of the class we want to create
             clsOrder AnOrder = new clsOrder();
@@ -44,7 +86,20 @@ namespace Testing4
         }
 
         [TestMethod]
-        public void NoOfCasesOrder()
+        public void DateAddedMin()
+        {
+            // create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            // create some test data
+            DateTime TestData = DateTime.Now.Date.AddYears(-1);
+            string Error = "";
+            Error = AnOrder.DateAddedValid(TestData);
+            // test to see if the two values are the same
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void NoOfCasesOK()
         {
             // create an instance of the class we want to create
             clsOrder AnOrder = new clsOrder();
@@ -57,7 +112,7 @@ namespace Testing4
         }
 
         [TestMethod]
-        public void ProductIDOrder()
+        public void ProductIDOK()
         {
             // create an instance of the class we want to create
             clsOrder AnOrder = new clsOrder();
@@ -69,7 +124,8 @@ namespace Testing4
             Assert.AreEqual(AnOrder.ProductID, TestData);
         }
 
-        public void AddressOrder()
+        [TestMethod]
+        public void AddressOK()
         {
             // create an instance of the class we want to create
             clsOrder AnOrder = new clsOrder();
@@ -81,7 +137,139 @@ namespace Testing4
             Assert.AreEqual(AnOrder.Address, TestData);
         }
 
-        public void CustomerIDOrder()
+        [TestMethod]
+        public void AddressValid()
+        {
+            // create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            // create some test data
+            string TestData = "17 Arminia Street, Leicester, LE4 6ER";
+            //create error message
+            string Error = "";
+            Error = AnOrder.AddressValid(TestData);
+            // test to see if the two values are the same
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void AddressMinLessOne()
+        {
+            // create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            // create some test data
+            string TestData = "";
+            //create error message
+            string Error = "";
+            Error = AnOrder.AddressValid(TestData);
+            // test to see if the two values are the same
+            Assert.AreNotEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void AddressMin()
+        {
+            // create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            // create some test data
+            string TestData = "1";
+            //create error message
+            string Error = "";
+            Error = AnOrder.AddressValid(TestData);
+            // test to see if the two values are the same
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void AddressMinPlusOne()
+        {
+            // create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            // create some test data
+            string TestData = "17";
+            //create error message
+            string Error = "";
+            Error = AnOrder.AddressValid(TestData);
+            // test to see if the two values are the same
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void AddressMaxLessOne()
+        {
+            // create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            // create some test data
+            string TestData = "";
+            TestData = TestData.PadLeft(99, '*');
+            //create error message
+            string Error = "";
+            Error = AnOrder.AddressValid(TestData);
+            // test to see if the two values are the same
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void AddressMax()
+        {
+            // create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            // create some test data
+            string TestData = "";
+            TestData = TestData.PadLeft(100, '*');
+            //create error message
+            string Error = "";
+            Error = AnOrder.AddressValid(TestData);
+            // test to see if the two values are the same
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void AddressMaxPlusOne()
+        {
+            // create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            // create some test data
+            string TestData = "";
+            TestData = TestData.PadLeft(101, '*');
+            //create error message
+            string Error = "";
+            Error = AnOrder.AddressValid(TestData);
+            // test to see if the two values are the same
+            Assert.AreNotEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void AddressMid()
+        {
+            // create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            // create some test data
+            string TestData = "";
+            TestData = TestData.PadLeft(50, '*');
+            //create error message
+            string Error = "";
+            Error = AnOrder.AddressValid(TestData);
+            // test to see if the two values are the same
+            Assert.AreEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void AddressExtreme()
+        {
+            // create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            // create some test data
+            string TestData = "";
+            TestData = TestData.PadLeft(5000, '*');
+            //create error message
+            string Error = "";
+            Error = AnOrder.AddressValid(TestData);
+            // test to see if the two values are the same
+            Assert.AreNotEqual(Error, "");
+        }
+
+        [TestMethod]
+        public void CustomerIDOK()
         {
             // create an instance of the class we want to create
             clsOrder AnOrder = new clsOrder();
@@ -93,7 +281,8 @@ namespace Testing4
             Assert.AreEqual(AnOrder.CustomerID, TestData);
         }
 
-        public void OrderNoOrder()
+        [TestMethod]
+        public void OrderNoOK()
         {
             // create an instance of the class we want to create
             clsOrder AnOrder = new clsOrder();
