aed0e3100b715a46a4be9be245057f80be7df588
Baran Adcock <P2665641@my365.dmu.ac.uk>
2023-03-24 19:40:42 +0000

    Added Validation to Middle Layer (Late Push)

---
+++ Testing1/tstCustomerCollection.cs
@@ -0,0 +1,23 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Framework
+{
+    [TestClass]
+    public class tstCustomerCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            // Creates an instance of the Class specified below
+            clsCustomerCollection AllCustomers = new clsCustomerCollection();
+            // Checks existence
+            Assert.IsNotNull(AllCustomers);
+        }
+    }
+}
