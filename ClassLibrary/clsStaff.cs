aade05926a8f285a48c33a2d6b0bd71969b807b8
Talhah Waraich <P2660958@my365.dmu.ac.uk>
2023-04-06 17:12:20 +0100

    completed the middle layer- fully working!

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -1,6 +1,15 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStaff
     {
+        public bool ShiftConfirmation { get; set; }
+        public int StaffID { get; set; }
+        public string StaffName { get; set; }
+        public string StaffPhoneNo { get; set; }
+        public string StaffPayroll { get; set; }
+        public DateTime Attendance { get; set; }
+        public string Schedule { get; set; }
     }
 }
