82f0fa621f5736967508a2c8b2cff5ff8efc6159
Michael Akintunde <P2669531@my365.dmu.ac.uk>
2023-04-17 21:52:55 +0100

    Added Find method to the class and creating tests for the find method using each property

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -4,11 +4,115 @@ namespace ClassLibrary
 {
     public class clsStock
     {
-        public int ProductID { get; set; }
-        public string ProductName { get; set; }
-        public int StockQuantity { get; set; }
-        public DateTime StockArrivalDate { get; set; }
-        public int TrackingID { get; set; }
-        public bool ProductAvailability { get; set; }
+        //Private member variables
+        private Int32 mProductID;
+        private string mProductName;
+        private Boolean mProductAvailability;
+        private Int32 mStockQuantity;
+        private DateTime mStockArrivalDate;
+        private Int32 mTrackingID;
+
+        //ProductID public property
+        public int ProductID
+        {
+            get
+            {
+                //return the private data
+                return mProductID;
+            }
+            set
+            {
+                //set the private data
+                mProductID = value;
+            }
+        }
+
+        //ProductName public property
+        public string ProductName
+        {
+            get
+            {
+                //return the private data
+                return mProductName;
+            }
+            set
+            {
+                //set the private data
+                mProductName = value;
+            }
+        }
+
+        //ProductAvailability public property
+        public bool ProductAvailability
+        {
+            get
+            {
+                //return the private data
+                return mProductAvailability;
+            }
+            set
+            {
+                //set the private data
+                mProductAvailability = value;
+            }
+        }
+
+         //StockQuantity public property
+        public int StockQuantity
+        {
+            get
+            {
+                //return the private data
+                return mStockQuantity;
+            }
+            set
+            {
+                //set the private data
+                mStockQuantity = value;
+            }
+        }
+
+        //StockArrivalDate public property
+        public DateTime StockArrivalDate
+        {
+            get
+            {
+                //return the private data
+                return mStockArrivalDate;
+            }
+            set
+            {
+                //set the private data
+                mStockArrivalDate = value;
+            }
+        }
+
+        //TrackingID public property
+        public int TrackingID
+        {
+            get
+            {
+                //return the private data
+                return mTrackingID;
+            }
+            set
+            {
+                //set the private data
+                mTrackingID = value;
+            }
+        }
+
+        public bool Find(int productID)
+        {
+            //Set the private data members to the test data value
+            mProductID = 21;
+            mProductName = "Test Name";
+            mProductAvailability = true;
+            mStockQuantity = 1;
+            mStockArrivalDate = Convert.ToDateTime("16/09/2015");
+            mTrackingID = 1;
+            //Always return true
+            return true;
+        }
     }
 }
