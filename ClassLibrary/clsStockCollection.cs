cacdaf41f19e8369c67894bf29c8bd69e1d99ffe
Michael Akintunde <P2669531@my365.dmu.ac.uk>
2023-04-25 20:58:13 +0100

    Added collection class and tests fro the class

---
+++ ClassLibrary/clsStockCollection.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class clsStockCollection
+    {
+        //Private data member for the list
+        List<clsStock> mStockList = new List<clsStock>();
+
+        //Public property for StockList
+        public List<clsStock> StockList
+        {
+            get
+            {
+                //Return private data
+                return mStockList;
+            }
+            set
+            {
+                //Set private data
+                mStockList = value;
+            }
+        }
+
+        //Public property for Count
+        public int Count
+        {
+            get
+            {
+                //Return the count on the list
+                return mStockList.Count;
+            }
+            set
+            {
+                //Set private data
+            }
+        }
+
+        //Constructor for the class
+        public clsStockCollection()
+        {
+            //Variable for Index
+            Int32 Index = 0;
+            //Variable to store Record Count
+            Int32 RecordCount = 0;
+            //Object for SQL Data Connection
+            clsDataConnection DB = new clsDataConnection();
+            //Execute the stored procedure
+            DB.Execute("sproc_tblSupplier_SelectAll");
+            //Gets the count of records
+            RecordCount = DB.Count;
+            //While there are records to process
+            while (Index < RecordCount)
+            {
+                //Create a blank stock
+                clsStock AStock = new clsStock();
+                //Read in the properties from the database record
+                AStock.ProductID = Convert.ToInt32(DB.DataTable.Rows[Index]["ProductID"]);
+                AStock.ProductName = Convert.ToString(DB.DataTable.Rows[Index]["ProductName"]);
+                AStock.ProductAvailability = Convert.ToBoolean(DB.DataTable.Rows[Index]["Availability"]);
+                AStock.StockQuantity = Convert.ToInt32(DB.DataTable.Rows[Index]["StockQnt"]);
+                AStock.StockArrivalDate = Convert.ToDateTime(DB.DataTable.Rows[Index]["StockArrivDate"]);
+                AStock.TrackingID = Convert.ToInt32(DB.DataTable.Rows[Index]["TrackingID"]); 
+                //Add the record to the private data member
+                mStockList.Add(AStock);
+                //Increase the index to point at the next record
+                Index++;
+            }
+        }
+
+        public clsStock ThisStock { get; set; }
+    }
+}
---
+++ Testing3/tstStockCollection.cs
@@ -0,0 +1,90 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstStockCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //Create an instance of the class we want to create
+            clsStockCollection AllStock = new clsStockCollection();
+            //Test to see if it exist
+            Assert.IsNotNull(AllStock);
+        }
+
+        [TestMethod]
+        public void StockListOK()
+        {
+            //Creates an instance of the class we want to create
+            clsStockCollection AllStock = new clsStockCollection();
+            //Assigns test data to the property
+            List<clsStock> TestList = new List<clsStock>();
+            //Create test data to add to the list
+            clsStock TestItem = new clsStock();
+            //Setting the properties of each item
+            TestItem.ProductID = 1;
+            TestItem.ProductName = "Jujutsu Kaisen: Yuji Itadori Iphone 11 Case";
+            TestItem.ProductAvailability = true;
+            TestItem.StockQuantity = 80;
+            TestItem.StockArrivalDate = DateTime.Now.Date;
+            TestItem.TrackingID = 5;
+            //Add the test items to the test list
+            TestList.Add(TestItem);
+            //Assign test data to the property
+            AllStock.StockList = TestList;
+            // Tests to see if both values are the same
+            Assert.AreEqual(AllStock.StockList, TestList);
+        }
+
+        [TestMethod]
+        public void ThisStockOK()
+        {
+            //Creates an instance of the class we want to create
+            clsStockCollection AllStock = new clsStockCollection();
+            //Create test data to add to assign to property
+            clsStock TestStock = new clsStock();
+            //Setting the properties of each item
+            TestStock.ProductID = 1;
+            TestStock.ProductName = "Jujutsu Kaisen: Yuji Itadori Iphone 11 Case";
+            TestStock.ProductAvailability = true;
+            TestStock.StockQuantity = 80;
+            TestStock.StockArrivalDate = DateTime.Now.Date;
+            TestStock.TrackingID = 5;
+            //Assign test data to the property
+            AllStock.ThisStock = TestStock;
+            // Tests to see if both values are the same
+            Assert.AreEqual(AllStock.ThisStock, TestStock);
+        }
+
+        [TestMethod]
+        public void ListandCountOK()
+        {
+            //Creates an instance of the class we want to create
+            clsStockCollection AllStock = new clsStockCollection();
+            //Assigns test data to the property
+            List<clsStock> TestList = new List<clsStock>();
+            //Create test data to add to the list
+            clsStock TestItem = new clsStock();
+            //Setting the properties of each item
+            TestItem.ProductID = 1;
+            TestItem.ProductName = "Jujutsu Kaisen: Yuji Itadori Iphone 11 Case";
+            TestItem.ProductAvailability = true;
+            TestItem.StockQuantity = 80;
+            TestItem.StockArrivalDate = DateTime.Now.Date;
+            TestItem.TrackingID = 5;
+            //Add the test items to the test list
+            TestList.Add(TestItem);
+            //Assign test data to the property
+            AllStock.StockList = TestList;
+            // Tests to see if both values are the same
+            Assert.AreEqual(AllStock.Count, TestList.Count);
+        }
+
+
+    }
+}
