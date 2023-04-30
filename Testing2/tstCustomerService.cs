4b9d3d859c11ae87166506279625dd37af4d964c
VizzyNsg <P2698451@my365.dmu.ac.uk>
2023-05-01 00:29:50 +0100

    I finished the your github day

---
+++ Testing2/tstCustomerService.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstCustomerService
+    {
+        [TestMethod]
+       public void InstanceOk()
+        {
+            //create an instance of the new class we want to create
+            clsCustomerService ACustomerService = new clsCustomerService();
+            //test to see that it exists
+            Assert.IsNotNull(ACustomerService);
+        }
+    }
+}
